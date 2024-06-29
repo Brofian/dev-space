b004584ec16513a3d7f3a1dfac0df611e3f3e963
Fabian Holzwarth <holzwarth.fabian@freenet.de>
2024-06-29 16:38:18 +0200

    feat: updating listme version

--- dev-app/src/app/screens/miscScreen/gadgets/ListmeApp.tsx
+++ dev-app/src/app/screens/miscScreen/gadgets/ListmeApp.tsx
@@ -32,9 +32,9 @@ export default class ListmeApp extends Component<{}, {}> {
                     <tbody>
                     <tr>
                         <td>Android</td>
-                        <td>v1.2.3</td>
+                        <td>v1.3.0</td>
                         <td>
-                            <a href="https://storage.fabianholzwarth.de/listme_v1_2_3.apk" target={"_blank"}>Download</a>
+                            <a href="https://storage.fabianholzwarth.de/listme_v1_3_0.apk" target={"_blank"}>Download</a>
                         </td>
                     </tr>
                     <tr>
